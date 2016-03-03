eb5cf731c7f4f55c35a25b21023633de6a6a3ba6
Thomas Bernfeld <happpyier@gmail.com>
2016-03-03 10:39:41 -0500

    Lets find the IP ADDRESS!

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('port', (process.env.PORT || 5000));
 //client,_consuming,_dumped,next,baseUrl,originalUrl,_parsedUrl,params,query,res,route
 app.get('/', function(request, response) {
 	var requestObjects = Object.keys(request.client.server);
-	var requestedItem = request.connection.client.server;
+	var requestedItem = request.connection.client;
 	response.send('Welcome to my rhb--microservice page<br/>The request objects are<br/>'+requestObjects+'<br/>The request item is<br/>'+requestedItem); 
 });
 app.listen(app.get('port'), function() {
