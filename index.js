d2ff8ed1be5cfb7d535bdb592fa07c6649749551
Thomas Bernfeld <happpyier@gmail.com>
2016-03-03 10:00:35 -0500

    installing initial files

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require("path");
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  response.end('Welcome to my rhb--microservice page'); 
+  response.send('Welcome to my rhb--microservice page'); 
 });
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
