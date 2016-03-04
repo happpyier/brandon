8e4d2bd44ee46262464cd9453502a7730ea3cba0
Thomas Bernfeld <happpyier@gmail.com>
2016-03-03 23:25:23 -0500

    Updated stuff from home

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var express = require('express');
-var app = express();
+var app = express(); 
 var path = require("path");
 var os = require('os');
 app.set('port', (process.env.PORT || 5000));
 //format is {"ipaddress":"107.144.151.227","language":"en-US","software":"Windows NT 6.1; WOW64"}
-//_readableState,readable,domain,_events,_maxListeners,socket,connection,httpVersionMajor,httpVersionMinor,httpVersion,
-//complete,headers,rawHeaders,trailers,rawTrailers,_pendings,_pendingIndex,upgrade,url,method,statusCode,statusMessage,
-//client,_consuming,_dumped,next,baseUrl,originalUrl,_parsedUrl,params,query,res,route
 app.get('/', function(request, response) {
 	var requestObjects = Object.keys(request.client.server);
-	var requestedItem = os.networkInterfaces();
-	response.send('Welcome to my rhb--microservice page<br/>The request objects are<br/>'+requestObjects+'<br/>The request item is<br/>'+requestedItem); 
+	var prerequestedItem = os.networkInterfaces()
+	var requestedItem = JSON.stringify(prerequestedItem);
+	var netAddress = JSON.stringify(prerequestedItem.eth0[0].address);
+	response.send('Welcome to my rhb--microservice page<br/>The request objects are<br/>'+requestObjects+'<br/>The netAddress is<br/>'+netAddress); 
 });
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
