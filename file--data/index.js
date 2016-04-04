884be0113ea846d47d6a058a4f4c948330693372
Thomas Bernfeld <happpyier@gmail.com>
2016-04-04 15:22:23 -0400

    setting up

--- file--data/index.js
+++ file--data/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
 https://developers.google.com/custom-search/docs/tutorial/introduction USING this as a reference;
 https://developers.google.com/custom-search/docs/element#supported_attributes Also a reference;
@@ -97,3 +98,21 @@ app.get('/:id', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port')); 
 });  
+=======
+var express = require('express');
+var app = express(); 
+var path = require("path");
+var os = require('os');
+app.set('port', (process.env.PORT || 5000));
+//format is {"ipaddress":"107.144.151.227","language":"en-US","software":"Windows NT 6.1; WOW64"}
+app.get('/', function(request, response) {
+	var requestObjects = Object.keys(request.client.server);
+	var prerequestedItem = os.networkInterfaces()
+	var requestedItem = JSON.stringify(prerequestedItem);
+	var netAddress = JSON.stringify(prerequestedItem.eth0[0].address);
+	response.send('Welcome to my rhb--microservice page<br/>The request objects are<br/>'+requestObjects+'<br/>The netAddress is<br/>'+netAddress); 
+});
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+>>>>>>> 7083a9ee6999f0d907549224fc53b9bc908adb39
